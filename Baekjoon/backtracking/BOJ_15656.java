2ba36db3b7f4e5bad96c77a3389bba0ed03a8f52
Miseong Kim <miseongk@naver.com>
2024-02-18 17:57:48 +0900

    feat: BOJ 15656 N과 M (7)

---
+++ Baekjoon/backtracking/BOJ_15656.java
@@ -0,0 +1,43 @@
+package Baekjoon.backtracking;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class BOJ_15656 {
+
+    static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static final StringBuilder sb = new StringBuilder();
+    static int n;
+    static int m;
+    static int[] arr;
+    static int[] nums;
+
+    public static void main(final String[] args) throws IOException {
+        final String[] nm = br.readLine().split(" ");
+        n = Integer.parseInt(nm[0]);
+        m = Integer.parseInt(nm[1]);
+        nums = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .sorted()
+                .toArray();
+        arr = new int[m];
+        dfs(0);
+        System.out.print(sb);
+    }
+
+    private static void dfs(final int depth) {
+        if (depth == m) {
+            for (int i = 0; i < m; i++) {
+                sb.append(arr[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = 0; i < n; i++) {
+            arr[depth] = nums[i];
+            dfs(depth + 1);
+        }
+    }
+}
